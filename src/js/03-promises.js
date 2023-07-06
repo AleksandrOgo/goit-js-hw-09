6883d962a3ac880e1167592723b0c2d9711a9a58
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-07-06 13:49:10 +0300

    js9

---
+++ src/js/03-promises.js
@@ -0,0 +1,34 @@
+import Notiflix from 'notiflix';
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+const form = document.querySelector('.form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+  const firstDelay = parseInt(delayInput.value);
+  const step = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+  let currentDelay = firstDelay;
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, currentDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`:white_check_mark: Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`:x: Rejected promise ${position} in ${delay}ms`);
+      });
+    currentDelay += step;
+  }
+});
